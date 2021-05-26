cfe5fcef67b3ec5bb05665d759a772c60214251b
Mariie23 <markoncher@gmail.com>
2021-05-26 12:46:10 +0300

    Add server.js to install module

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
 var express = require("express"),
 	http = require("http"),
-	app = express(),
-	toDos = [
-		// настраиваем список задач копированием
-		// содержимого из файла todos.OLD.json
-	];
+	mongoose = require("mongoose"),
+	app = express();
+	
 app.use(express.static(__dirname + "/client"));
-http.createServer(app).listen(3000);
-// Этот маршрут замещает наш файл
-// todos.json в примере из части 5
-app.get("/todos.json", function (req, res) {
-	res.json(toDos);
-});
-// командуем Express принять поступающие
-// объекты JSON
-app.use(express.urlencoded({ extended: true }));
-app.post("/todos", function (req, res) {
-	// сейчас объект сохраняется в req.body
-	var newToDo = req.body;
-	console.log(newToDo);
-	toDos.push(newToDo);
-	// отправляем простой объект
-	res.json({"message":"Вы размещаетеся на сервере!"});
-});
+app.use(express.urlencoded());
+// подключаемся к хранилищу данных Amazeriffic в Mongo
+mongoose.connect('mongodb://localhost/amazeriffic');
