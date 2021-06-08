8a266bc77e190aa7e83b988cdd535837d1558f26
Mariie23 <markoncher@gmail.com>
2021-06-08 21:51:11 +0300

    Настройка маршрутов

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require("express"),
 	http = require("http"),
 	mongoose = require("mongoose"),
-	app = express(); /*,
+	usersController = require("./controllers/users_controller"),
+    toDosController = require("./controllers/todos_controller"),
+	ToDo = require("./models/todo"),
+	app = express();
+	mongoose.connect('mongodb://localhost/amazeriffic'); 
+	http.createServer(app).listen(3000);/*,
 	toDos = [
 		// настраиваем список задач копированием
 		// содержимого из файла todos.OLD.json
@@ -9,33 +14,22 @@ var express = require("express"),
 	// импортируем библиотеку mongoose
 app.use(express.static(__dirname + "/client"));
 // Это модель Mongoose для задач
-var ToDoSchema = mongoose.Schema({
-	description: String,
-	tags: [ String ]
-});
-var ToDo = mongoose.model("ToDo", ToDoSchema);
-http.createServer(app).listen(3000);
-// Этот маршрут замещает наш файл
-// todos.json в примере из части 5
 app.get("/todos.json", function (req, res) {
 	ToDo.find({}, function (err, toDos) {
-		// не забудьте о проверке на ошибки
-		res.json(toDos);
-	});
+        if (err !== null) {
+            // объект не был передан
+            console.log(err);
+            res.json({ "message": "объект не был передан!" });
+        } else { res.json(toDos); }
+    });
 });
+// Этот маршрут замещает наш файл
+// todos.json в примере из части 5
+
 // командуем Express принять поступающие
 // объекты JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 // подключаемся к хранилищу данных Amazeriffic в Mongo
-mongoose.connect('mongodb://localhost/Amazeriffic', {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true 
-	}).then(res => {
-		console.log("DB Connected!")
-	}).catch(err => {
-		console.log(Error, err.message);
-	});
 app.post("/todos", function (req, res) {
 	console.log(req.body);
 	var newToDo = new ToDo({"description":req.body.description,
@@ -57,3 +51,16 @@ app.post("/todos", function (req, res) {
 		}
 	});
 });
+app.use('/', express.static((__dirname + "/Client")));
+app.use('/user/:username', express.static(__dirname + '/client'));
+
+app.get("/users.json", usersController.index);
+app.post("/users", usersController.create);
+app.get("/users/:username", usersController.show);
+app.put("/users/:username", usersController.update);
+app.delete("/users/:username", usersController.destroy);
+
+app.get("/user/:username/todos.json", toDosController.index);
+app.post("/user/:username/todos", toDosController.create);
+app.put("/user/:username/todos/:id", toDosController.update);
+app.delete("/user/:username/todos/:id", toDosController.destroy);
